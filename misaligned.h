d378f12c2d5f0363b90dc52ad4b1dcc4a35dd234
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-01-25 19:52:09 +0530

    Create misaligned.h

---
+++ misaligned.h
@@ -0,0 +1,4 @@
+void printOnConsole(int MajorColorNumber, int MinorColorNumber);
+
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
