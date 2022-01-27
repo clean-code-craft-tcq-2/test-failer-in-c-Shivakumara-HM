8b17049fc114e12e93c8569521f98e20788b9a97
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-01-28 00:26:03 +0530

    Update misaligned.h

--- misaligned.h
+++ misaligned.h
@@ -1,8 +1,15 @@
-void printOnConsole(int Index, const char* MajorColor, const char* MinorColor);
-void testNumberToPair(int Index, const char* MajorColor, const char* MinorColor);
+ColorPair GetColorFromPairNumber(int pairNumber);
+void testNumberToPair(int pairNumber, const char* expectedMajor, const char* expectedMinor);
+int printColorMap() ;
 
 const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
 int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+typedef struct {
+    const char* majorColor;
+    const char* minorColor;
+} ColorPair;
+
