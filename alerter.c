02e22ddfda67302a4747352d5db4a96edd3849f8
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-02-01 12:46:19 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -40,7 +40,7 @@ int main()
     alertInCelcius(400.5);
     alertInCelcius(303.6);
     alertInCelcius(212.7);
-    assert(alertFailureCount == 2);
+    assert(alertFailureCount == 1);
     
     printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
